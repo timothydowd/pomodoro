d1c6ac015e2355b4ef9edbb2eb66439ff433c31d
dwdy250 <timdowd1984@gmail.com>
2018-07-07 20:32:06 +0100

    html and css done

---
+++ script.js
@@ -0,0 +1,11 @@
+// work timer counts down from 25 mins and adds a pomodoros when complete
+
+// then break timer counts down 5 mins
+
+// above is looped unless we are on any multiple of 4 pomodoros
+
+// then we take a break for 25 mins
+
+// then we loop above until stop is pressed
+
+// if pause is pressed at any time during work timer then work timer starts from beginning once we resume
