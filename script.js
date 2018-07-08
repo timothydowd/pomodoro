773459de9350a10a5a78d815d2d85ec25ea1354b
dwdy250 <timdowd1984@gmail.com>
2018-07-08 22:58:50 +0100

    basic counter setup, needs fixing to switch between work and break counts

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 // work timer counts down from 25 mins and adds a pomodoros when complete
+const workTimer = 5;
+const breakTimer = 10;
+const lbreakTimer = 15;
+var pomCounter = 0;
+var timer = workTimer;
+
+var mins, seconds;
+
+
+var counting = setInterval(function () {
+  mins = parseInt(timer / 60);
+  seconds = parseInt(timer % 60);
+
+  mins = mins < 10 ? "0" + mins : mins;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  console.log(mins + ":" + seconds);
+
+  timer = --timer;
+
+  if(timer < 0){
+    console.log("Pomodoros!");
+    pomCounter += 1;
+    timer = pomCounter % 4 == 0 ? lbreakTimer : breakTimer;
+    }
+
+
+
+
+
+}, 1000);
 
 // then break timer counts down 5 mins
 
